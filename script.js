1c4426c416b6c5c44aa7128ed496cf8b78a630cd
Atishay <atishaysoni1801@gmail.com>
2024-01-21 16:44:31 +0530

    files added

---
+++ script.js
@@ -0,0 +1,53 @@
+const apikey = "145b7f022691cabafc74406866d6d917";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+const url = (city) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeatherByLocation(city) {
+  const resp = await fetch(url(city), { origin: "cors" });
+  const respData = await resp.json();
+  addWeatherToPage(respData);
+}
+
+function addWeatherToPage(data) {
+  const temp = KtoC(data.main.temp);
+  const humidity = data.main.humidity;
+  const windSpeed = data.wind.speed;
+
+  const weather = document.createElement("div");
+  weather.classList.add("weather");
+
+  weather.innerHTML = `
+        <h2><img src="https://openweathermap.org/img/wn/${
+          data.weather[0].icon
+        }@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${
+    data.weather[0].icon
+  }@2x.png" /></h2>
+        <small>${data.weather[0].main}</small>
+        <div class="more-info">
+        <p>Humidity : <span>${humidity}%</span></p>
+        <p>Wind speed : <span>${Math.trunc(windSpeed * 3.6)} km/h</span></p>
+        </div>
+    `;
+
+  main.innerHTML = "";
+  main.appendChild(weather);
+}
+
+function KtoC(K) {
+  return Math.floor(K - 273.15);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const city = search.value;
+
+  if (city) {
+    getWeatherByLocation(city);
+  }
+});
